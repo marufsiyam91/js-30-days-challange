c7dbefd6b7fc2465dd683c412befcd621278b59c
Maruf Siyam <marufsiyam9123@gmail.com>
2024-07-14 23:44:09 +0600

    add task 9 to 12

--- operators.js
+++ operators.js
@@ -53,4 +53,41 @@ function compare(num1, num2) {
 
 console.log(compare(20, 35))
 
+
+//Task 09:  Write a program to compare two numbers using >= and <=  and log the result to the console.
+function compareNums(num1, num2) {
+    const isSmall = num1 <= num2
+    const isBig = num1 >= num2
+
+    return {
+        isSmall,
+        isBig
+    }
+}
+
+console.log(compareNums(20, 20))
+
+//Task 10:  Write a program to compare two numbers using == and ===  and log the result to the console.
+function checkValueandType(value1, value2) {
+    console.log(value1 == value2)
+    console.log(value1 === value2)
+}
+
+console.log(checkValueandType(20, '20'))
+
+//Task 11:  Write a program that uses the &&  operator to combine two conditions and log the result to the console.
+function conditionCheck(value1, value2) {
+    if (value1 > value2 && value1 > 25) return value1
+}
+
+console.log(conditionCheck(30, 15))
+
+//Task 12:  Write a program that uses the ||  operator to combine two conditions and log the result to the console.
+function CheckOr(value1, value2) {
+    if (value1 > value2 || value1 > 10) return value1
+}
+
+console.log(CheckOr(12, 15))
+
+
  
