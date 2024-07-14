8ededdefb1690d3437514bd083af98f2d095c653
Maruf Siyam <marufsiyam9123@gmail.com>
2024-07-14 23:43:31 +0600

    add task 5 to 8

--- operators.js
+++ operators.js
@@ -21,4 +21,36 @@ function devideNums(a, b) {
     return a / b
 }
 console.log(devideNums(5, 9))
+
+//Task 05:  Write a program to find the remainder when one number is divided by another and log the result to the console.
+function reminderNums(a, b) {
+    return a % b
+}
+console.log(reminderNums(9, 5))
+
+// Task 06: use the += operator to add a number to a variable and log the result to the console.
+let number = 25;
+number += 25;
+
+console.log(number)
+
+//Task 07: use the -= operator to subtract a number from a variable and log the result to the console.
+let anotherNumber = 50;
+anotherNumber -= 25;
+
+console.log(anotherNumber)
+
+//Task 08:  Write a program to compare two numbers using > and < and log the result to the console.
+function compare(num1, num2) {
+    const isSmall = num1 < num2
+    const isBig = num1 > num2
+
+    return {
+        isSmall,
+        isBig
+    }
+}
+
+console.log(compare(20, 35))
+
  
