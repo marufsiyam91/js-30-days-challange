769e03a57b5c266b0d2e1c19e1f1bd0aead19729
Maruf Siyam <marufsiyam9123@gmail.com>
2024-07-14 23:42:46 +0600

    add 1 to 4 task

---
+++ operators.js
@@ -0,0 +1,24 @@
+// Task 01:  Write a program to add two numbers and log the result to the console.
+function addNums(a, b) {
+    return a + b
+}
+console.log(addNums(5, 9))
+
+//Task 02:  Write a program to substract two numbers and log the result to the console.
+function substractNums(a, b) {
+    return a - b
+}
+console.log(substractNums(5, 9))
+
+//Task 03:  Write a program to multiply two numbers and log the result to the console.
+function multiplyNums(a, b) {
+    return a * b
+}
+console.log(multiplyNums(5, 9))
+
+//Task 04:  Write a program to devide two numbers and log the result to the console.
+function devideNums(a, b) {
+    return a / b
+}
+console.log(devideNums(5, 9))
+ 
