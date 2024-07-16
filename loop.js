b9dae63607e049ef8298257815a0bf2b4d1c477e
Maruf Siyam <marufsiyam9123@gmail.com>
2024-07-16 23:09:50 +0600

    add task 1 to 3

---
+++ loop.js
@@ -0,0 +1,21 @@
+// Task 01: Print numbers from 1 to 10 using a for loop.
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
+
+// Task 02: Print the multiplication table of 5 using a for loop.
+const number = 5;
+for (let i = 1; i <= 10; i++) {
+  console.log(`${number} x ${i} = ${number * i}`);
+}
+
+// Task 03: Calculate the sum of numbers from 1 to 10 using a while loop.
+let sum = 0;
+let i = 1;
+
+while (i <= 10) {
+  sum += i;
+  i++;
+}
+console.log("Sum of numbers from 1 to 10 is:", sum);
+
