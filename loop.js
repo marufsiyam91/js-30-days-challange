ed2912ea091cb886f0211eddc840b8e5b686aaf7
Maruf Siyam <marufsiyam9123@gmail.com>
2024-07-16 23:10:27 +0600

    add task 04 to 06

--- loop.js
+++ loop.js
@@ -19,3 +19,31 @@ while (i <= 10) {
 }
 console.log("Sum of numbers from 1 to 10 is:", sum);
 
+// Task 04: Print numbers from 10 to 1 using a while loop.
+i = 10;
+while (i >= 1) {
+  console.log(i);
+  i--;
+}
+
+// Task 05: Print numbers from 1 to 5 using a do...while loop.
+i = 1; // Re-initialize the variable
+do {
+  console.log(i);
+  i++;
+} while (i <= 5);
+
+// Task 06: Calculate the factorial of a number using a do...while loop.
+function factorial(n) {
+  let result = 1;
+  let i = n;
+
+  do {
+    result *= i;
+    i--;
+  } while (i > 0);
+
+  return result;
+}
+console.log(`Factorial of ${number} is: ${factorial(number)}`);
+
